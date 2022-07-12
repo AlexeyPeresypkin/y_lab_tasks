50bd1d3f71ba731db1715a57599716014b7a161f
Alexey Peresypkin <aepre3@gmail.com>
2022-07-12 19:30:21 +0300

    refactor try_repeat func

--- hw_3/hw_3.py
+++ hw_3/hw_3.py
@@ -65,7 +65,7 @@ def try_repeat(call_count, start_sleep_time, factor, border_sleep_time):
                 print(f'Запуск номер {number}. '
                       f'Ожидание: {start_sleep_time} сек. '
                       f'Результат декорируемой функций = {result}')
-                start_sleep_time *= 2 ** factor
+                start_sleep_time *= factor
                 if border_sleep_time <= start_sleep_time:
                     start_sleep_time = border_sleep_time
             print('Конец работы')
@@ -75,7 +75,7 @@ def try_repeat(call_count, start_sleep_time, factor, border_sleep_time):
     return inner
 
 
-@try_repeat(call_count=3, start_sleep_time=1, factor=2, border_sleep_time=10)
+@try_repeat(call_count=10, start_sleep_time=1, factor=2, border_sleep_time=10)
 def test_func():
     return 'Результат декорируемой функции'
 
