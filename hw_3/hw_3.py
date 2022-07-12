b24b0706c277110c45c6079ce57c3a16db7cd81c
Alexey Peresypkin <aepre3@gmail.com>
2022-07-12 20:18:51 +0300

    refactor try_repeat

--- hw_3/hw_3.py
+++ hw_3/hw_3.py
@@ -58,16 +58,17 @@ def try_repeat(call_count, start_sleep_time, factor, border_sleep_time):
 
         def wrapper(*args, **kwargs):
             nonlocal call_count, start_sleep_time
+            t = start_sleep_time
             print(f'Кол-во запусков = {call_count}\nНачало работы')
             for number in range(1, call_count + 1):
-                time.sleep(start_sleep_time)
+                time.sleep(t)
                 result = func(*args, **kwargs)
                 print(f'Запуск номер {number}. '
-                      f'Ожидание: {start_sleep_time} сек. '
+                      f'Ожидание: {t} сек. '
                       f'Результат декорируемой функций = {result}')
-                start_sleep_time *= factor
-                if border_sleep_time <= start_sleep_time:
-                    start_sleep_time = border_sleep_time
+                t = start_sleep_time * factor ** number
+                if border_sleep_time <= t:
+                    t = border_sleep_time
             print('Конец работы')
 
         return wrapper
